39d4bb92e18fe123bb4ee7796f98b8b1988fccbe
Vaishnavi5758 <vaishnavipatil5758@gmail.com>
2023-10-28 22:18:47 +0530

    Arrow fun vs simple fun, objects

--- Hello.js
+++ Hello.js
@@ -1,1 +1,20 @@
 console.log("Hello from node js");
+
+
+
+const c = (a,b) =>a*b;
+
+console.log(c(5,6));
+
+
+const student =
+{
+    name : 'vaishnavi',
+    surname :'patil',
+
+    info(){
+        console.log('Hi my name is' + this.name +this.surname); //this refers to global scope hence if we use arrow fun here, 
+        //it wont takr local values, hence we used simple function defn
+    }
+}
+student.info();
