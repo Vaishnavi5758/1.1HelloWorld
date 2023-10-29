e98e8b796665790bfeeca51a9b731f265623f700
Vaishnavi5758 <vaishnavipatil5758@gmail.com>
2023-10-29 20:32:25 +0530

    destructuring

--- Hello.js
+++ Hello.js
@@ -52,3 +52,44 @@ const obj4 = { ...obj3, key1: 1000}
 console.log(obj3)
 
 console.log(obj4)
+
+//QUESTION
+
+// const obj11 = {'key11': 1, "key12": 2, "key13": 1000}
+
+// const { key11, key13} = { ...obj11}
+
+
+
+// console.log(key11, key13)
+
+// const obj1 = {'key1': 1, "key2": 2, "key3": 1000}
+
+// let { key1, key3} = obj1
+
+
+
+const obj11 = {'key1': 50, "key2": 2, "key3": 60}
+
+let { key1, key3} = obj11
+
+
+
+key1 = 20;
+
+key3 = 123
+
+console.log(obj11.key1, obj11.key3)
+
+
+//
+const arr1 = ['value1', 'value2']
+
+const [ val1, val2 ] = arr1
+
+
+
+console.log(val1)
+
+console.log(val2)
+
