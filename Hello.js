b76922a35b9465692dba2f79b18f8badadf37c6a
Vaishnavi5758 <vaishnavipatil5758@gmail.com>
2023-10-29 19:01:55 +0530

    Arrays Spread and rest operator

--- Hello.js
+++ Hello.js
@@ -18,3 +18,37 @@ const student =
     }
 }
 student.info();
+
+
+const originalArray = ['apple', 'oranges', ' ', 'mango', ' ', 'lemon'];
+
+const transformedArray = originalArray.map(item => {
+  if (item.trim() === '') {
+    return 'empty string';
+  } else {
+    return item;
+  }
+});
+
+console.log(transformedArray);
+
+//QUESTION
+const obj1 = {'key1': 1}
+const obj2 = { ...obj1}
+if(obj2 === obj1){
+console.log('same objects')
+}
+else{
+console.log('different objects')
+}
+
+//QUESTION
+const obj3 = {'key1': 1 , 'key2' : 2}
+
+const obj4 = { ...obj3, key1: 1000}
+
+
+
+console.log(obj3)
+
+console.log(obj4)
