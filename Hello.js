48d7aa57b5b3ccb853a41d07fee29553779a4bf5
Vaishnavi5758 <vaishnavipatil5758@gmail.com>
2023-10-30 11:36:11 +0530

    async await

--- Hello.js
+++ Hello.js
@@ -93,3 +93,26 @@ console.log(val1)
 
 console.log(val2)
 
+
+
+//
+
+function delayPrint(letter, delay) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        console.log(letter);
+        resolve();
+      }, delay);
+    });
+  }
+  
+  async function printSequence() {
+    await delayPrint('a', 1000);
+    await delayPrint('b', 1000);
+    await delayPrint('c', 1000);
+    await delayPrint('d', 1000);
+    await delayPrint('e', 1000);
+  }
+  
+  printSequence();
+  
